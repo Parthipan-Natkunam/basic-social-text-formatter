b14919d22e5f8fa696d1b41d38de8168ce71baf3
Parthipan-Natkunam <pnatkunam@babbel.com>
2024-12-06 23:59:16 +0100

    refactor: use the native webcomponent directly in the html

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
-import "./app.css";
 import RichTextEditor from "./lib/components/RichTextEditor.svelte";
 
-const container = document.getElementById("app");
-
-if(container){
-    container.innerHTML = `
-    <social-post-formatter/>
-    `;
-} else {
-    console.error("Container not found");
-    document.body.innerHTML =
-      "<p class='load-error'>⚠️ <strong>Error:</strong> Container Missing. Contact The Admin</p>";
-}
+export default RichTextEditor;
