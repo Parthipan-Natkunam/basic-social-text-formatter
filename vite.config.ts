b603397bd560301e8cfda3d4e8f8b2dbf116357e
Parthipan-Natkunam <pnatkunam@babbel.com>
2024-12-27 01:29:53 +0100

    tests: add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  base: './',
-  plugins: [svelte({
-    configFile: 'svelte.config.js'
-  })],
-})
+  base: "./",
+  plugins: [
+    svelte({
+      configFile: "svelte.config.js",
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTest.ts"],
+  },
+});
